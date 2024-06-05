3bbd8c78b4030b79f158e1ae51f7f024fb611f43
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-06-05 18:06:03 +0200

    Add analytics notebook

--- 04-stats-parties.py
+++ 04-stats-parties.py
@@ -7,27 +7,33 @@ import re
 # Lire les données à partir du fichier CSV
 df = pd.read_csv("./data/raw/candidats-eur-2024.csv", delimiter=";")
 
-pattern = re.compile(r'\(\d+\) - ')
+pattern = re.compile(r"\(\d+\) - ")
 
 # Parcourir les parties et générer les camemberts
-for partie, partie_data in df.groupby('Numéro de panneau'):
+for partie, partie_data in df.groupby("Numéro de panneau"):
     # Créer un dossier pour la partie
     dossier_partie = f"./data/stats/{partie}"
     os.makedirs(dossier_partie, exist_ok=True)
 
-    partie_data['Profession'] = partie_data['Profession'].str.replace(pattern, '')
+    partie_data["Profession"] = partie_data["Profession"].str.replace(
+        pattern, "", regex=True
+    )
 
     # Générer le camembert pour les professions de cette partie
-    profession_counts = partie_data['Profession'].value_counts()
+    profession_counts = partie_data["Profession"].value_counts()
 
     plt.figure(figsize=(12, 8))
-    profession_counts.plot(kind='pie', autopct=lambda p: f'{p:.1f}%', startangle=140)
-    plt.title(f'Répartition des professions')
-    plt.axis('equal')  # Assurez-vous que le camembert est un cercle
+    profession_counts.plot(kind="pie", autopct=lambda p: f"{p:.1f}%", startangle=140)
+    plt.title(f"Répartition des professions")
+    plt.axis("equal")  # Assurez-vous que le camembert est un cercle
 
     # Enregistrer le graphique dans le dossier de la partie
     # plt.savefig(os.path.join(dossier_partie, f'camembert_professions_partie.png'))
-    plt.savefig(os.path.join(dossier_partie, f'camembert_professions_partie.svg'), format='svg', dpi=300)
+    plt.savefig(
+        os.path.join(dossier_partie, f"camembert_professions_partie.svg"),
+        format="svg",
+        dpi=300,
+    )
     plt.close()
 
     # Calculate percentages
@@ -36,19 +42,23 @@ for partie, partie_data in df.groupby('Numéro de panneau'):
     # Prepare the age distribution with percentages for JSON
     profession_distribution_dict = profession_percentages.round(1).to_dict()
 
-    with open(os.path.join(dossier_partie, f'repartition_professions_partie.json'), 'w') as json_file:
+    with open(
+        os.path.join(dossier_partie, f"repartition_professions_partie.json"), "w"
+    ) as json_file:
         json.dump(profession_distribution_dict, json_file, indent=4, ensure_ascii=False)
 
-    parite_counts = partie_data['Sexe'].value_counts()
+    parite_counts = partie_data["Sexe"].value_counts()
 
     plt.figure(figsize=(12, 8))
-    parite_counts.plot(kind='pie', autopct=lambda p: f'{p:.1f}%', startangle=140)
-    plt.title(f'Parité')
-    plt.axis('equal')  # Assurez-vous que le camembert est un cercle
+    parite_counts.plot(kind="pie", autopct=lambda p: f"{p:.1f}%", startangle=140)
+    plt.title(f"Parité")
+    plt.axis("equal")  # Assurez-vous que le camembert est un cercle
 
     # Enregistrer le graphique dans le dossier de la partie
     # plt.savefig(os.path.join(dossier_partie, f'camembert_professions_partie.png'))
-    plt.savefig(os.path.join(dossier_partie, f'camembert_parite.svg'), format='svg', dpi=300)
+    plt.savefig(
+        os.path.join(dossier_partie, f"camembert_parite.svg"), format="svg", dpi=300
+    )
     plt.close()
 
     # Calculate percentages
@@ -57,33 +67,45 @@ for partie, partie_data in df.groupby('Numéro de panneau'):
     # Prepare the age distribution with percentages for JSON
     parite_distribution_dict = parite_percentages.round(1).to_dict()
 
-    with open(os.path.join(dossier_partie, f'repartition_parite.json'), 'w') as json_file:
+    with open(
+        os.path.join(dossier_partie, f"repartition_parite.json"), "w"
+    ) as json_file:
         json.dump(parite_distribution_dict, json_file, indent=4, ensure_ascii=False)
 
     # Convertir la colonne 'Date de naissance' en datetime
-    partie_data['Date de naissance'] = pd.to_datetime(partie_data['Date de naissance'], format='%d/%m/%Y')
+    partie_data["Date de naissance"] = pd.to_datetime(
+        partie_data["Date de naissance"], format="%d/%m/%Y"
+    )
 
     # Calculer l'âge à partir de la date de naissance
-    partie_data['Age'] = pd.Timestamp.now().year - partie_data['Date de naissance'].dt.year
+    partie_data["Age"] = (
+        pd.Timestamp.now().year - partie_data["Date de naissance"].dt.year
+    )
 
     # Définir les tranches d'âges
     bins = [18, 30, 40, 50, 60, 70, 120]
-    labels = ['18-29', '30-39', '40-49', '50-59', '60-69', '70+']
+    labels = ["18-29", "30-39", "40-49", "50-59", "60-69", "70+"]
 
     # Ajouter une colonne pour les tranches d'âges
-    partie_data['Tranche d\'âge'] = pd.cut(partie_data['Age'], bins=bins, labels=labels, right=False)
+    partie_data["Tranche d'âge"] = pd.cut(
+        partie_data["Age"], bins=bins, labels=labels, right=False
+    )
 
     # Générer le camembert pour les tranches d'âge de cette partie
-    age_counts = partie_data['Tranche d\'âge'].value_counts()
+    age_counts = partie_data["Tranche d'âge"].value_counts()
 
     plt.figure(figsize=(8, 6))
-    age_counts.plot(kind='pie', autopct=lambda p: f'{p:.1f}%', startangle=140)
-    plt.title(f'Répartition des tranches d\'âge')
-    plt.axis('equal')  # Assurez-vous que le camembert est un cercle
+    age_counts.plot(kind="pie", autopct=lambda p: f"{p:.1f}%", startangle=140)
+    plt.title(f"Répartition des tranches d'âge")
+    plt.axis("equal")  # Assurez-vous que le camembert est un cercle
 
     # Enregistrer le graphique dans le dossier de la partie
     # plt.savefig(os.path.join(dossier_partie, f'camembert_tranches_age_partie.png'))
-    plt.savefig(os.path.join(dossier_partie, f'camembert_tranches_age_partie.svg'), format='svg', dpi=300)
+    plt.savefig(
+        os.path.join(dossier_partie, f"camembert_tranches_age_partie.svg"),
+        format="svg",
+        dpi=300,
+    )
     plt.close()
 
     # Calculate percentages
@@ -92,5 +114,7 @@ for partie, partie_data in df.groupby('Numéro de panneau'):
     # Prepare the age distribution with percentages for JSON
     age_distribution_dict = age_percentages.round(1).to_dict()
     # Round to match the pie chart display
-    with open(os.path.join(dossier_partie, f'repartition_tranches_age.json'), 'w') as json_file:
+    with open(
+        os.path.join(dossier_partie, f"repartition_tranches_age.json"), "w"
+    ) as json_file:
         json.dump(age_distribution_dict, json_file, indent=4, ensure_ascii=False)
--- 06-search-keyword-tech-ia.py
+++ 06-search-keyword-tech-ia.py
@@ -1,12 +1,12 @@
 import json
 from collections import Counter
 
-def find_keyword(numPanneau, pathName, keywords):
 
+def find_keyword(numPanneau, pathName, keywords):
     if numPanneau == 13:
         print("ff")
 
-    path = "./data/program/"+str(numPanneau)+"/"+pathName+".pdf.md"
+    path = "./data/program/" + str(numPanneau) + "/" + pathName + ".pdf.md"
 
     # Convertir la liste de mots-clés en un ensemble pour une recherche rapide
     keywords_set = set(keywords)
@@ -14,7 +14,7 @@ def find_keyword(numPanneau, pathName, keywords):
     total_count = 0
 
     # Ouvrir le fichier et lire ligne par ligne
-    with open(path, 'r', encoding='utf-8') as file:
+    with open(path, "r", encoding="utf-8") as file:
         for line in file:
             # Compter les occurrences de chaque mot dans la ligne
             words = line.split()
@@ -24,64 +24,77 @@ def find_keyword(numPanneau, pathName, keywords):
 
     return total_count
 
+
 # Lecture du fichier JSON depuis un fichier
-with open('./data/refacto/programme-website-stats-falc.json', 'r', encoding='utf-8') as file:
+with open(
+    "./data/refacto/programme-website-stats-falc.json", "r", encoding="utf-8"
+) as file:
     data = json.load(file)
 
 # Nouvelle liste pour stocker les données mises à jour
 updated_data = []
-keywordTech = ["réseaux sociaux", "cyber", "cyberdéfense", "l'IA", "(IA)", "cyberattaques", "cybersécurité", "intelligence", "artificielle", "DSA",
-               "RGPD", "numerique", "numériques", "datacenter", "open source","TikTok"
-               "données personnelles"]
+keywordTech = [
+    "réseaux sociaux",
+    "cyber",
+    "cyberdéfense",
+    "l'IA",
+    "(IA)",
+    "cyberattaques",
+    "cybersécurité",
+    "intelligence",
+    "artificielle",
+    "DSA",
+    "RGPD",
+    "numerique",
+    "numériques",
+    "datacenter",
+    "open source",
+    "TikTok" "données personnelles",
+]
 
 # Parcourir chaque entrée dans les données
-for entry in data['data']:
+for entry in data["data"]:
+    entry["hasTech"] = 0
 
-    entry['hasTech'] = 0
-
-    if entry['pdf_acc'] != "0":
-        occurenceTechKeyword = find_keyword(entry['numPanneau'], entry['pdf_acc'], keywordTech)
+    if entry["pdf_acc"] != "0":
+        occurenceTechKeyword = find_keyword(
+            entry["numPanneau"], entry["pdf_acc"], keywordTech
+        )
         if occurenceTechKeyword:
-            entry['hasTech'] = {
-                "name": 'pdf_acc',
-                "occurence": occurenceTechKeyword
-            }
+            entry["hasTech"] = {"name": "pdf_acc", "occurence": occurenceTechKeyword}
 
-    if entry['hasTech'] == 0 and entry['falc_acc'] != "0":
-        occurenceTechKeyword = find_keyword(entry['numPanneau'], entry['falc_acc'], keywordTech)
+    if entry["hasTech"] == 0 and entry["falc_acc"] != "0":
+        occurenceTechKeyword = find_keyword(
+            entry["numPanneau"], entry["falc_acc"], keywordTech
+        )
         if occurenceTechKeyword:
-            entry['hasTech'] = {
-                "name": 'falc_acc',
-                "occurence": occurenceTechKeyword
-            }
+            entry["hasTech"] = {"name": "falc_acc", "occurence": occurenceTechKeyword}
 
-    if entry['hasTech'] == 0 and entry['pdf'] != "0":
-        occurenceTechKeyword = find_keyword(entry['numPanneau'], entry['pdf'], keywordTech)
+    if entry["hasTech"] == 0 and entry["pdf"] != "0":
+        occurenceTechKeyword = find_keyword(
+            entry["numPanneau"], entry["pdf"], keywordTech
+        )
         if occurenceTechKeyword:
-            entry['hasTech'] = {
-                "name": 'pdf',
-                "occurence": occurenceTechKeyword
-            }
+            entry["hasTech"] = {"name": "pdf", "occurence": occurenceTechKeyword}
 
-    if entry['hasTech'] == 0 and entry['falc'] != "0":
-        occurenceTechKeyword = find_keyword(entry['numPanneau'], entry['falc'], keywordTech)
+    if entry["hasTech"] == 0 and entry["falc"] != "0":
+        occurenceTechKeyword = find_keyword(
+            entry["numPanneau"], entry["falc"], keywordTech
+        )
         if occurenceTechKeyword:
-            entry['hasTech'] = {
-                "name": 'falc',
-                "occurence": occurenceTechKeyword
-            }
+            entry["hasTech"] = {"name": "falc", "occurence": occurenceTechKeyword}
 
     # Ajouter l'entrée mise à jour à la nouvelle liste
     updated_data.append(entry)
 
 # Nouveau dictionnaire avec les données mises à jour
-updated_json = {
-    "data": updated_data
-}
+updated_json = {"data": updated_data}
 
 # Conversion du dictionnaire en chaîne JSON
 updated_json_str = json.dumps(updated_json, indent=4)
 
 # Écriture des données mises à jour dans le fichier JSON
-with open("data/refacto/programme-website-stats-falc-keyword-tech.json", 'w', encoding='utf-8') as f:
+with open(
+    "data/refacto/programme-website-stats-falc-keyword-tech.json", "w", encoding="utf-8"
+) as f:
     json.dump(updated_json, f, ensure_ascii=False, indent=4)
